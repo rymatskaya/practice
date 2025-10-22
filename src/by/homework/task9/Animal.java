38309d02f4dc72f266423e0c19a3fd2d44ce0ee6
Irina Rymatskaya <irina.rymatskaya@gmail.com>
2025-10-22 21:03:47 +0300

    Наследование

---
+++ src/by/homework/task9/Animal.java
@@ -0,0 +1,88 @@
+package by.homework.task9;
+
+import java.util.Objects;
+
+/**
+ * Поля в классе животное:
+ * * окрас, максимальная продолжительность жизни, тип еды которой питаются (мясо, насекомые, растения).
+ * * Определить методы в классе животное: (издание звуков, животное играет)
+ */
+public abstract class Animal extends Zoo {
+    private static int count = 0;
+    private String color;
+    private int lifetime;
+    private String foodtype;
+    private int id;
+
+    public Animal(String color, int lifetime, String foodtype) {
+        this.id = ++count;
+        this.color = color;
+        this.lifetime = lifetime;
+        this.foodtype = foodtype;
+        System.out.println("Конструктор с параметрами класса Животное");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Animal() {
+        System.out.println("Конструктор класса Животное");
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getLifetime() {
+        return lifetime;
+    }
+
+    public void setLifetime(int lifetime) {
+        this.lifetime = lifetime;
+    }
+
+    public String getFoodtype() {
+        return foodtype;
+    }
+
+    public void setFoodtype(String foodtype) {
+        this.foodtype = foodtype;
+    }
+
+    public void makeSound() {
+        System.out.println("Животное издает звук");
+    }
+
+    public void play() {
+        System.out.println("Животное играет");
+    }
+
+    public String toString() {
+        String result = "";
+        result = result + "id: " + id +" окрас: " + color +
+                ", максимальная продолжительность жизни: " + lifetime +
+                ", тип еды, которой питаются: " + foodtype;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Animal animal)) return false;
+        return getLifetime() == animal.getLifetime() && getId() == animal.getId() && Objects.equals(getColor(), animal.getColor()) && Objects.equals(getFoodtype(), animal.getFoodtype());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getColor(), getLifetime(), getFoodtype(), getId());
+    }
+}
---
+++ src/by/homework/task9/Dog.java
@@ -0,0 +1,98 @@
+package by.homework.task9;
+
+import by.homework.task9.Animal;
+
+/**
+ * В классе собака определить имя,
+ * * порода, средний вес. В классе Собака реализовать методы: лаять, кусать, бегать, играть, прыгать.
+ * * При этом, в консоли должны выводится имя собаки вместе с описанием действия (пример. Рекс прыгает).
+ */
+public class Dog extends Animal {
+    private String name;
+    private String breed;
+    private Double averageWeight;
+
+    public Dog(String color, int lifetime, String foodtype, String name, String breed, Double averageWeight) {
+        super(color, lifetime, foodtype);
+        this.name = name;
+        this.breed = breed;
+        this.averageWeight = averageWeight;
+        System.out.println("Конструктор с параметрами класса Собака");
+    }
+
+    public Dog() {
+        super();
+        System.out.println("Конструктор класса Собака");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Double getAverageWeight() {
+        return averageWeight;
+    }
+
+    public void setAverageWeight(Double averageWeight) {
+        this.averageWeight = averageWeight;
+    }
+
+    public void makeSound() {
+        System.out.println(name + " лает");
+    }
+
+    public void bite() {
+        System.out.println(name + " кусает");
+    }
+
+    public void run() {
+        System.out.println(name + " бежит");
+    }
+
+    @Override
+    public void play() {
+        System.out.println(name + " играет в мяч");
+    }
+
+    public void jump() {
+        System.out.println(name + " прыгает");
+    }
+
+    public String toString() {
+        String result = super.toString();
+        result = result + " Имя: " + name +
+                ", порда: " + breed +
+                ", средний возраст: " + averageWeight;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Dog)) {
+            return false;
+        }
+        Dog dog = (Dog) o;
+        if (name.equals(dog.getName()) && breed.equals(dog.getBreed()) &&
+                Double.compare(averageWeight, dog.averageWeight) == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
